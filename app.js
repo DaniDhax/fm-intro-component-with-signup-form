22997bc1e95a895ff9af5e7a85a2bd361b8a42b1
DaniDhax <DaniDhax@users.noreply.github.com>
2023-09-04 19:14:28 +0200

    Mensajes de error en cada campo

--- app.js
+++ app.js
@@ -1,28 +1,41 @@
-const $formulario = document.getElementById("formulario");
-const $customAlert = document.getElementById("custom-alert");
+const $btnSumit = document.querySelector("#btn-submit");
+const $camposFormulario = document.querySelectorAll(".campo");
 
-$formulario.addEventListener("submit", (e) => {
+let regex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+let formularioCorrecto = true;
+
+function comprobarCampos(e) {
   e.preventDefault();
-  let isEmpty = false;
-
-  document.querySelectorAll("input").forEach((input) => {
-    if (input.value === "" && isEmpty === false) {
-      $customAlert.style.display = "block";
-      const $customAlertMessage = document.querySelector(".custom-alert-content p");
-      $customAlertMessage.innerText = `${input.attributes.placeholder.value} cannot be empty`
-
-      // alert(`${input.attributes.placeholder.value} cannot be empty`);
-      isEmpty = true;
+  $camposFormulario.forEach((campo) => {
+    if (campo.value === "") {
+      campo.style.border = "2px solid var(--my-red)";
+      document.getElementById(`icon-${campo.id}`).style.visibility = "visible";
+      document.getElementById(`message-${campo.id}`).style.display = "block";
+      formularioCorrecto = false;
+    } else {
+      campo.style.border = "1px solid var(--grayish-blue)";
+      document.getElementById(`icon-${campo.id}`).style.visibility = "hidden";
+      document.getElementById(`message-${campo.id}`).style.display = "none";
+      formularioCorrecto = true;
     }
   });
+}
 
-  if (isEmpty) {
-    return;
+$btnSumit.addEventListener("click", (e) => {
+  comprobarCampos(e);
+  if (
+    document.getElementById("email").value !== "" &&
+    !regex.test(document.getElementById("email").value)
+  ) {
+    document.getElementById(`icon-email`).style.visibility = "visible";
+    document.getElementById(`message-email`).style.display = "block";
+    document.getElementById(`message-email`).innerText =
+      "Looks like this is not an email";
+    formularioCorrecto = false;
+  } 
+
+  if (formularioCorrecto) {
+    console.log ("Formulario correcto");
   }
 
-  console.log("Se envía el formulario");
 });
-
-function closeCustomAlert() {
-  $customAlert.style.display = "none";
-}
