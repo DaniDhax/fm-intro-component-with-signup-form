3612623293143e4f0df84fb53dddaea295fd360e
DaniDhax <DaniDhax@users.noreply.github.com>
2023-09-04 14:56:22 +0200

    Mensaje de alerta superior

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 const $formulario = document.getElementById("formulario");
+const $customAlert = document.getElementById("custom-alert");
 
 $formulario.addEventListener("submit", (e) => {
   e.preventDefault();
   let isEmpty = false;
+
   document.querySelectorAll("input").forEach((input) => {
     if (input.value === "" && isEmpty === false) {
-        alert(`${input.attributes.placeholder.value} cannot be empty`);
-        isEmpty = true;
+      $customAlert.style.display = "block";
+      const $customAlertMessage = document.querySelector(".custom-alert-content p");
+      $customAlertMessage.innerText = `${input.attributes.placeholder.value} cannot be empty`
+
+      // alert(`${input.attributes.placeholder.value} cannot be empty`);
+      isEmpty = true;
     }
   });
 
-  if (isEmpty) {return};
+  if (isEmpty) {
+    return;
+  }
 
   console.log("Se envía el formulario");
 });
+
+function closeCustomAlert() {
+  $customAlert.style.display = "none";
+}
